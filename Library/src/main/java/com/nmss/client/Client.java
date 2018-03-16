9ef4acbca7ae1a5e11166013873dd6f630c17a9f
Varun <Varun@DESKTOP-SQSI4SD>
2018-03-16 18:43:26 +0530

    client with CER

---
+++ Library/src/main/java/com/nmss/client/Client.java
@@ -0,0 +1,127 @@
+package com.nmss.client;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class Client {
+	private Socket clientSocket;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private String serverIP;
+	private int serverPort;
+	private String localIP;
+	private int localPort;
+
+	public Client(String serverIP, int serverPort, String localIP, int localPort) {
+		this.serverIP = serverIP;
+		this.serverPort = serverPort;
+		this.localIP = localIP;
+		this.localPort = localPort;
+	}
+
+	public void connect() {
+		try {
+			clientSocket = new Socket(serverIP, serverPort, InetAddress.getByName(localIP), localPort);
+			in = new DataInputStream(clientSocket.getInputStream());
+			out = new DataOutputStream(clientSocket.getOutputStream());
+			System.out.println("connection created with server....");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendRequest(byte data[]) {
+		try {
+			out.write(data);
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public byte[] readFromServer()
+	{
+		byte length [] = new  byte[3];
+		byte version [] = new  byte[1];
+		byte data[] = new byte[20000];
+		byte total_data[] = null;
+		try
+		{
+			in.read(data,0,1);
+			in.read(data,1,3);
+			version[0]      = data[0];
+			length[0]       = data[1];
+			length[1]       = data[2];
+			length[2]       = data[3];
+			int total_length_to_receive     = convertToInt(convertToHex(length));
+			total_data = new  byte[total_length_to_receive];
+			int received_from_server = readComplete((total_length_to_receive-4),data,4,0);
+			int ll = received_from_server+4;
+			if(total_length_to_receive == ll)
+			{
+				System.arraycopy(data,0, total_data, 0,total_length_to_receive);
+			}
+			else
+			{
+				int bytes_left_to_read = total_length_to_receive-(received_from_server+4);
+			}
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return total_data;
+	}
+
+	public int readComplete(int total_length_to_receive, byte data[], int startIndex,int count) throws Exception
+	{
+		int readed = in.read(data,startIndex,total_length_to_receive);
+		count = count+readed;
+		if(readed == total_length_to_receive)
+		{
+//			BillingClient.LW.writeIntoLog(2," REQUEST MDN ["+MSISDN+"] I have readed complete in one frame count ["+count+"]");
+		}
+		else
+		{
+			int bytes_left_to_read = total_length_to_receive - readed;
+//			BillingClient.LW.writeIntoLog(2," REQUEST MDN ["+MSISDN+"] I have readed only ["+readed+"] GOING TO READ AGAIN.. LEFT ["+bytes_left_to_read+"]");
+			return readComplete(bytes_left_to_read,data,(readed+4),count);
+			//readComplete(data,(readed+1)bytes_left_to_read);
+		}
+		return count;
+	}
+
+	public String convertToHex(byte[] data) 
+	{
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < data.length; i++) 
+		{
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do 
+			{
+				if ((0 <= halfbyte) && (halfbyte <= 9))
+					buf.append((char) ('0' + halfbyte));
+				else
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} 
+			while(two_halfs++ < 1);
+		}
+		return buf.toString();
+	}
+
+	public int convertToInt(String data)
+	{
+		return Integer.parseInt(data, 16);
+	}
+
+	public void start() {
+		connect();
+	}
+}
---
+++ Library/src/main/java/com/nmss/util/Main.java
@@ -0,0 +1,31 @@
+package com.nmss.util;
+
+import com.nmss.client.Client;
+import com.nmss.messages.CEAMessage;
+import com.nmss.messages.CERMessage;
+
+import dk.i1.diameter.AVP;
+import dk.i1.diameter.AVP_Unsigned32;
+
+public class Main 
+{
+	public static void main(String aa[]) throws Exception
+	{
+		if (aa[0].equals("client"))
+		{
+			Client client = new Client("103.206.248.235",8888,"103.206.248.235",4444);
+			client.start();
+			CERMessage cer = new CERMessage(1, 2, "103.206.248.235", "B", "5", "D", 3);
+			System.out.println("CERMessage is created.....");
+			client.sendRequest(cer.encode());
+			System.out.println("CERMessage is send.....");
+			byte received [] = client.readFromServer();
+			System.out.println("CEA is received.......");
+			CEAMessage answer = new CEAMessage();
+			answer.decode(received);
+			AVP avp = answer.find(268);
+			int resultcode = new AVP_Unsigned32(avp).queryValue();
+			System.out.println("CER Answer ["+resultcode+"]");
+		}
+	}
+}
